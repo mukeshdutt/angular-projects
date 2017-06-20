c4c581bb945a55d9ef0f23472ea5e93dbae4ad2d
mukeshdutt <mukeshdutt5@gmail.com>
2017-06-21 03:21:33 +0530

    git search application with oauth application client id and client secret

--- github-profile-search/app/services/github.services.ts
+++ github-profile-search/app/services/github.services.ts
@@ -4,18 +4,20 @@ import { Http } from "@angular/http";
 @Injectable()
 export class GithubService {
 
-    username: string;
-    
+    private username: string;
+    private clientId: string = "38d394802feeaf5a703f";
+    private clientSecret = "07e0122e97cf6d17fb38c4ca6caad447a00c1cf0";
+
     constructor(private http: Http) {
         this.username = 'mukeshdutt';
     }
 
     GetUser() {
-        return this.http.get('http://api.github.com/users/' + this.username);
+        return this.http.get('http://api.github.com/users/' + this.username + "?client_id=" + this.clientId + '&client_secret=' + this.clientSecret);
     }
 
     GetRepos() {
-        return this.http.get('http://api.github.com/users/' + this.username + '/repos');
+        return this.http.get('http://api.github.com/users/' + this.username + '/repos' + "?client_id=" + this.clientId + '&client_secret=' + this.clientSecret);
     }
 
     UpdateUser(username: string) {
