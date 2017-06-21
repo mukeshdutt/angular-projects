dbcc92a44f0a52343c59c95851215dbd6a31013c
mukeshdutt <mukeshdutt5@gmail.com>
2017-06-22 01:13:34 +0530

    add service file and register that into main module

--- spotify-app/app/app.module.ts
+++ spotify-app/app/app.module.ts
@@ -1,18 +1,22 @@
+//  call angular modules
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-
 import { FormsModule } from '@angular/forms'
 import { RouterModule } from '@angular/router'
 import { HttpModule } from '@angular/http'
 
+// calling components
 import { AppComponent } from './app.component'
 import { AboutComponent } from './component/about/about.component'
 import { NavbarComponent } from './component/navbar/navbar.component'
 
+// Calling services
+import { SpotifyService } from './services/spotify.services'
+
 @NgModule({
     declarations: [AppComponent, NavbarComponent, AboutComponent],
-    imports: [BrowserModule],
+    imports: [BrowserModule, HttpModule, FormsModule, RouterModule],
     bootstrap: [AppComponent],
-    providers: []
+    providers: [SpotifyService]
 })
 export class AppModule { }
