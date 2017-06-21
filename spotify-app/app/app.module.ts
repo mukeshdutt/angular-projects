ff0d7f737068f4c53c33804c3224da57f938b04e
mukeshdutt <mukeshdutt5@gmail.com>
2017-06-22 01:09:29 +0530

    call angular modules for use in different in components and services etc.

--- spotify-app/app/app.module.ts
+++ spotify-app/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
+import { FormsModule } from '@angular/forms'
+import { RouterModule } from '@angular/router'
+import { HttpModule } from '@angular/http'
+
 import { AppComponent } from './app.component'
 import { AboutComponent } from './component/about/about.component'
 import { NavbarComponent } from './component/navbar/navbar.component'
