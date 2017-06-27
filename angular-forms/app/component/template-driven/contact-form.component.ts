200cc6eaef92193a8016978e8a1c9e7862ca324a
mukeshdutt <mukeshdutt5@gmail.com>
2017-06-27 15:33:23 +0530

    template driven form in angular 2

--- angular-forms/app/component/template-driven/contact-form.component.ts
+++ angular-forms/app/component/template-driven/contact-form.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms'
 
 @Component({
     moduleId: module.id,
     selector: 'contact',
-    templateUrl: 'contact-form.component.html'
+    templateUrl: 'contact-form.component.html',
+    styles: [`
+        //.ng-invalid { border:1px solid red; }
+    `]
 })
-export class ContactFormComponent { }
+export class ContactFormComponent {
+
+    private user = {
+        Username: 'mukesh',
+        Email: 'mukeshdutt5@gmail.com',
+        Comment: 'Welcome to goexamples',
+        Gender: 'male'
+    };
+
+    genders = ['male', 'female']
+
+    onSubmit(form: NgForm) {
+        console.log(form.value);
+    }
+}
